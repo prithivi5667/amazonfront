8bb0e413c22bc9c47b76e7139e3a3cad0321c7a8
prithivi5667 <prithivi5667@gmail.com>
2021-12-27 21:32:38 +0530

    link update

--- src/Axios.js
+++ src/Axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-    baseURL: "https://shopclonept.herokuapp.com/"
+    baseURL: "https://shopclonept.herokuapp.com"
 })
 
 export default instance
